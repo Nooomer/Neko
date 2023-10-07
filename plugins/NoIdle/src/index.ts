38fe8bbae9bca9a9f7468be522a03645a6a2e5f0
Nooomer <ampilogov.qaz@gmail.com>
2023-10-07 16:08:50 +0500

    fix

--- plugins/NoIdle/src/index.ts
+++ plugins/NoIdle/src/index.ts
@@ -2,7 +2,7 @@ import { patcher } from "@vendetta";
 import { FluxDispatcher } from "@vendetta/metro/common";
 
 export const onUnload = patcher.before("dispatch", FluxDispatcher, ([{ type }]) => {
-    if (type !== "IDLE") return [{ type: "OFFLINE", idle: false }];
+    if (type !== "IDLE") return;
 
-    return;
+    return [{ type: "IDLE", idle: false }];
 });
